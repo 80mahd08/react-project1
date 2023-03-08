5040b12dd288a22a68b90d9a896d124f1857eb16
80mahd08 <amarimahdi8@gmail.com>
2023-03-08 18:03:33 +0100

    add contact file

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+    return (
+        <div>
+            <input id="email" type='text' placeholder="type your email"/>
+            <textarea id="text"></textarea>
+            <input id="btnSub" type="submit"/>
+        </div>
+    );
+}
+
+export default Contact;
